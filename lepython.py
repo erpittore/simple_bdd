c3d5c4b4c9867f43b048365da66144c43fdbbd69
Alex Manzanares <alexandermanzanares6@gmail.com>
2022-03-07 17:18:37 -0800

    python has been added

---
+++ lepython.py
@@ -0,0 +1,34 @@
+thistuple = ('pomme', 'pommme de terre', 'cerise', 'ananas')
+print(thistuple)
+#####################
+tuple1 = ('pomme', 'orange', 'fraise')
+tuple2 = (1, 2, 3 ,2, 1, 1)
+tuple3 = (True, False, True)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+###################################
+x = 3+5j
+y= 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
